d1f83ce0998edc606fa6be45f9b39094388fa86e
Antonio Jesus Pinilla Belmez <antonio@MacBook-Pro-de-Antonio.local>
2018-08-07 18:22:36 +0100

    Se añade la lógica de programación y las llamadas a la API

--- app/src/main/java/com/antonio/whattheweather/MainActivity.java
+++ app/src/main/java/com/antonio/whattheweather/MainActivity.java
@@ -1,13 +1,144 @@
 package com.antonio.whattheweather;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText cityName;
+    TextView resultTextView;
+
+    public void findWeather(View view) {
+
+        Log.i("cityName", cityName.getText().toString());
+
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://api.openweathermap.org/data/2.5/weather?q=" + cityName.getText().toString() + "&APPID=e5e00c51e469f3742b57a8f25651c958");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityName = findViewById(R.id.cityName);
+        resultTextView = findViewById(R.id.resultTextView);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            } catch (Exception e) {
+
+                Toast.makeText(getApplicationContext(), "Enter a valid city", Toast.LENGTH_LONG);
+
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                String message = "";
+
+                JSONObject jsonObject = new JSONObject(result);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Website content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < arr.length(); i++) {
+
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description =  jsonPart.getString("description");
+
+                    if (main != "" && description != "") {
+
+                        message += ": " + description + "\r\n";
+
+                    }
+
+
+                }
+
+                if (message != ""){
+
+                    resultTextView.setText(message);
+                } else  {
+
+                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+                }
+
+            } catch (JSONException e) {
+
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+            }
+
+
+        }
     }
 }
